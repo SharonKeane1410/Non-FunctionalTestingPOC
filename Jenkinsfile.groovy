7bc2f30b01772239634e3467a1edcd3231dc913f
SharonKeane1410 <shazkeane335@gmail.com>
2020-01-30 16:36:07 +0000

    changes to the jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,10 @@
-node {
-   stage('') {
-      //
-   }
-   stage('Run Postman collection with Taurus newman executor') {
+pipeline {
+    stage('Run Postman collection with Taurus newman executor') {
         bat 'docker run --rm ' +
                 '-v JMeter/Taurus/newman_executor/bzt-config:/bzt-configs ' +
                 '-v JMeter/Taurus/newman_executor/artifacts:/tmp/artifacts ' +
                 'blazemeter/taurus ' +
                 'JMeter/Taurus/newman_executor/bzt-config/run_postman_collection.yml'
-   }
 
-   stage('Run JMeter test plan with Taurus default JMeter executor') {
-
-   }
+    }
 }
