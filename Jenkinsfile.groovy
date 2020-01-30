c258e6fd25ce28aa0a980e81bf2c369eae151a7e
SharonKeane1410 <shazkeane335@gmail.com>
2020-01-30 16:45:12 +0000

    First test for jenkins server

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
     stages{
         stage('Run Postman collection with Taurus newman executor') {
-            step{
+            steps{
                 bat 'docker run --rm ' +
                         '-v JMeter/Taurus/newman_executor/bzt-config:/bzt-configs ' +
                         '-v JMeter/Taurus/newman_executor/artifacts:/tmp/artifacts ' +
