6fbc7597583dcb2a272c5d3df8f8a1ddff5cf214
SharonKeane1410 <shazkeane335@gmail.com>
2020-01-30 16:40:28 +0000

    Fixing missing sections in jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,14 @@
 pipeline {
-    stage('Run Postman collection with Taurus newman executor') {
-        bat 'docker run --rm ' +
-                '-v JMeter/Taurus/newman_executor/bzt-config:/bzt-configs ' +
-                '-v JMeter/Taurus/newman_executor/artifacts:/tmp/artifacts ' +
-                'blazemeter/taurus ' +
-                'JMeter/Taurus/newman_executor/bzt-config/run_postman_collection.yml'
+    stages{
+        stage('Run Postman collection with Taurus newman executor') {
+            bat 'docker run --rm ' +
+                    '-v JMeter/Taurus/newman_executor/bzt-config:/bzt-configs ' +
+                    '-v JMeter/Taurus/newman_executor/artifacts:/tmp/artifacts ' +
+                    'blazemeter/taurus ' +
+                    'JMeter/Taurus/newman_executor/bzt-config/run_postman_collection.yml'
+
+        }
 
     }
+
 }
